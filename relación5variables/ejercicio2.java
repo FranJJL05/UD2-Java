043bc1fe046aa50a68280b3ed28d96c097f40875
FranJJL05 <fjimlop182@g.educaand.es>
2024-10-31 12:12:42 +0100

    Add files via upload

---
+++ relación5variables/ejercicio2.java
@@ -0,0 +1,70 @@
+package relación5variables;
+
+public class ejercicio2 {
+    // 2-
+    // a)
+    byte edad = 19;
+
+    // b)
+    int codigopostal = 19902;
+
+    // c)
+    float altura = 1.82f;
+
+    // d)
+    char Hombre = 'H';
+    char Mujer = 'M';
+
+    // e)
+    String nombre = "Fran";
+
+    // f)
+    byte nhijos = 2;
+
+    // g)
+    float iva = 3;
+
+    // h)
+    byte tallacamisa = 40;
+
+    // i)
+    float peso = 75.2f;
+
+    // j)
+    float precio = 1.0;
+
+    // k)
+    boolean valor = true;
+    boolean valor = false;
+
+    // l)
+    String mensaje = "Hola";
+
+    // m)
+    char letra = 'a';
+
+    // n)
+    boolean mayordeedad = true;
+
+    // o)
+    byte minutos = 23;
+
+    // p)
+    int dias = 270;
+
+    // q)
+    String matricula = "6789MDSA";
+
+    // r)
+    int contador = 0;
+
+    // s)
+    boolean mayoredad = true;
+    boolean mayoredad = false;
+
+    // t)
+    String tallacamiseta = "S";
+    String tallacamiseta = "M";
+    String tallacamiseta = "L";
+    String tallacamiseta = "XL";
+}
